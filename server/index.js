676d7bdff4ed838d42d65db6fbc281ba35e0d438
satyam <satyamsingh17538@gmail,com>
2024-03-02 00:56:16 +0530

    3. Final

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://study-notion-frontend-roan.vercel.app/"],
+    origin: ["https://study-notion-frontend-roan.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
